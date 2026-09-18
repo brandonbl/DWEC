fa4403de21a9882efe0073d71dc53d63de592ad9
brandonbl <brandonlb53@educastur.es>
2026-09-18 14:10:59 +0200

    Delete EJEM_02/js directory

--- EJEM_02/js/25_IgualdadObjetos.js
+++
@@ -1,9 +0,0 @@
-let harry = { name: 'Harry Smith', age: 42 } 
-
-let harry2 = harry 
-
-harry === harry2 // verdadero: dos referencias al mismo objeto 
-
-let harry3 = { name: 'Harry Smith', age: 42 } 
-
-harry === harry3 // falso: objetos diferentes 
